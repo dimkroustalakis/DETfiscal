5cd4c39b33810a34cd9bf3a0740f9aaacab56c01
dim kroustalakis <t8240068@aueb.gr>
2025-10-30 11:10:01 +0200

    Update message for test1 branch

--- TestThisGit.java
+++ TestThisGit.java
@@ -1,6 +1,6 @@
 public class TestThisGit {
     public static void main(String[] args) {  // String needs capital S, args is conventional
-        System.out.println("I am not loving this git thing"); // System capitalized, added semicolon
+        System.out.println("I CHANGED A THING IN BRANCH 1 EIIII"); // System capitalized, added semicolon
     }
 
 }
