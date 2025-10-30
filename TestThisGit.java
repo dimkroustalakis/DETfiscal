f46f823be20a2173ba9e7c3c143912eca7f8884a
dim kroustalakis <t8240068@aueb.gr>
2025-10-30 10:49:32 +0200

    Add TestThisGit.java with initial print statement

---
+++ TestThisGit.java
@@ -0,0 +1,6 @@
+public class TestThisGit {
+    public static void main(String[] args) {  // String needs capital S, args is conventional
+        System.out.println("I am not loving this git thing"); // System capitalized, added semicolon
+    }
+
+}
